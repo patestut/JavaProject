2735e807bb833e42941a4b955bf51236214933c2
Treesa17 <sajut@sheridancollege.ca>
2023-04-04 09:52:06 -0400

    Deliverable 1

---
+++ src/content/Employee.java
@@ -0,0 +1,49 @@
+package content;
+
+public class Employee {
+private int ID;
+private String Name;
+private String City;
+private String Position;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getPosition() {
+        return Position;
+    }
+
+    public void setPosition(String Position) {
+        this.Position = Position;
+    }
+
+    public Employee(int ID, String Name, String City, String Position) {
+        this.ID = ID;
+        this.Name = Name;
+        this.City = City;
+        this.Position = Position;
+    }
+
+
+}
---
+++ src/content/EmployeeFile.java
@@ -0,0 +1,26 @@
+package content;
+
+public class EmployeeFile {
+/*
+     public static ArrayList<Worker> getWorker(String fileName) throws FileNotFoundException {
+
+        ArrayList<Worker> workerList = new ArrayList();
+        File file = new File(fileName);
+        Scanner k = new Scanner(file);
+        while (k.hasNext()) {
+            // System.out.println(k.nextLine());
+            int ID = k.nextInt();
+            String first = k.next();
+            String last = k.next();
+            String name = first + " " + last;
+           // System.out.println(ID + " name is " + name);
+            Worker one=new Worker(ID);
+            one.setName(name);
+            workerList.add(one);
+        }
+        return workerList;
+
+    }/*n
+    */
+   
+}
